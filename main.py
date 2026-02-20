d5c23984aaad624e787a8111af12c636a07a4df7
profe-pako <pako@uabc.edu.mx>
2026-02-19 20:27:59 -0800

    add function to project

--- main.py
+++ main.py
@@ -36,32 +36,45 @@ def repetido(id):
 def borralo(id):
     index_borrar = False
     for id2 in range(rango):
-        print(f"ID2 ==> {id2}")
         if id == productos_identificadores[id2]:
             print(f"Encontrado {id2}")
             print(productos_identificadores[id2])
             print(productos_identificadores)
             index_borrar = id2  
+            print(f"\tEl valor de index_borrar = {index_borrar}")
     """Modulo para borrar"""
-    # print(f"El valor de index_borrar = {index_borrar}")
-    # if index_borrar:
-    #     x = productos_identificadores.pop(index_borrar)
-    #     print(f"Borrer al {x}")
-    # print(productos_identificadores)
+    if index_borrar:
+         x = productos_identificadores.pop(index_borrar)
+         print(f"\t Borrer al {x}")
+    else:
+        print("\t **Producto no encontrado**")
+    print(productos_identificadores)
 
 
-def agregar(id):
+def agregar():
     """ Seccion  para ingresar un nuevo producto"""
-    identificador = input("Dame el numero de id: ")
+    print("\tAgregando producto nuevo")
+    identificador = input("Dame el id: ")
     if repetido(identificador):
-        print("Producto ya existe")
+        print("\t __Producto ya existe__")
     else :
-        nombre = input("Dame el name: ")
-        cantidad = input("Dame la cantidad: ")
+        nombre = input("Nombre: ")
+        cantidad = input("Cantidad: ")
         productos_lista.append(nombre)
         productos_cantidades.append(cantidad)
         productos_identificadores.append(identificador)
+        print("\t--Producto Agregado--")
         print(productos_lista)
+
+
+
+identificador = input("Dame el codigo identificador del producto :: ")
+buscalo(identificador)
+identificador = input("Dame el codigo identificador del producto para borrarlo:: ")
+borralo(identificador)
+agregar()        
+print(productos_lista)
+
 #almacen = [productos_lista, productos_identificadores, productos_cantidades]
 # bandera = True
 # if rango != len(productos_identificadores):
@@ -76,12 +89,4 @@ def agregar(id):
 #     for p in range(rango):
 #         print(f"productos:{productos_identificadores[p]}:{productos_lista[p]},{productos_cantidades[p]}")
         
-
-identificador = input("Dame el codigo identificador del producto :: ")
-buscalo(identificador)
-identificador = input("Dame el codigo identificador del producto para borrarlo:: ")
-borralo(identificador)
-        
-
-
 
